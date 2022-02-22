b72e129a119d37c4db2466e9b214f6c96c10e28a
Oliver Angélil <olivermangelil@gmail.com>
2022-02-22 20:01:06 +0100

    Update 2_H-index.py

--- Google/KickStart/2_H-index.py
+++ Google/KickStart/2_H-index.py
@@ -9,34 +9,19 @@ case = 2
 N = 6
 citations_test = [1, 3, 3, 2, 2, 15]
 
-# function to solve the problem
-def h_index_calculator(papers: int, citations: List) -> str:
-    h_index = 0
-    rolling_citations = []
-    h_index_evolution = []
 
-    for paper in range(1, papers+1):
-        
-        paper_citation = citations[paper-1]
-        
-        if paper_citation > paper:
-            rolling_citations.append(paper_citation)
-            rolling_citations = [cit for cit in rolling_citations if cit > h_index]
-            if len(rolling_citations) > h_index:
-                h_index += 1
+# function to solve the problem                                                                                                                                                                             
+def h_index_calculator(citations: List) -> str:
+    papers = []
+    ans = []
+    hindex = 0
+    for i in citations:
+        papers.append(i)
+        if sorted(papers, reverse=True)[hindex] > hindex:
+            hindex+=1
+        ans.append(str(hindex))
 
-        elif paper_citation > h_index:
-            rolling_citations.append(paper_citation)
-            if len(rolling_citations) > h_index:
-                h_index += 1
-            
-            rolling_citations = [cit for cit in rolling_citations if cit > h_index]
-        
-        
-        h_index_evolution.append(h_index)
-
-    #return "Case #{}: {}".format(case, *h_index_evolution)
-    return ' '.join(map(str, h_index_evolution))
+    return ' '.join(ans)
     
 
 # Submission code
